7a0f4407d82f6c787f3cfec9cf65d57c8e7e0ae0
张中昊 <zhangzhonghao@tanwan.com>
2022-05-17 09:37:42 +0800

    F -

--- app/Check/Model/MysqlModel/CheckInfoZhuanShengBenModel.php
+++ app/Check/Model/MysqlModel/CheckInfoZhuanShengBenModel.php
@@ -7,11 +7,12 @@ use Library\Abstracts\Model\AbstractMySqlModel;
 
 class CheckInfoZhuanShengBenModel extends AbstractMySqlModel
 {
-    public $tableName = "checkinfo_zhuanShengben_";
+    public $tableName = "checkinfo_zhuanshengben_";
 
     public function check(ZhuanShengBenCheckObject $object)
     {
         $b = $this->builder->from($this->tableName . date('Y'))->where($object->toArray());
+        $b->orderBy('kao_shi_shi_jian');
         return $b->get();
     }
 }
